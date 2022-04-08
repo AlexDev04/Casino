083ac6605ea1de3ddee265fce9f70c42cb655dab
Aleksey.yakovlev <Web.Yakovlev04@yandex.ru>
2022-04-08 11:27:42 +0300

    Restart & result
    add: добавлена возможность перезапуска, выводится результат

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,7 +18,7 @@ const gamesList = ['Black Jack']
 gamesLinks.forEach((el) => {
     el.addEventListener('click', () => {
         games.forEach((el) => el.classList.remove('active'));
-        console.log(`Запущена игра ${gamesList[0]}`);
+        console.log(`Game ${gamesList[0]} started`);
         games[gamesLinks.indexOf(el)].classList.add('active');
     })
 })
@@ -26,57 +26,103 @@ gamesLinks.forEach((el) => {
 
 function BlackJack() {
 
-    const playerScoreEl = document.getElementsByClassName('BJ-scoreP')[0];
-    const robotScoreEl = document.getElementsByClassName('BJ-scoreR')[0];
+    const playerScoreEl = document.getElementsByClassName('BJ-score-P')[0];
+    const robotScoreEl = document.getElementsByClassName('BJ-score-R')[0];
+    const resultEl = document.getElementsByClassName('BJ-result')[0];
 
     const addCardBtn = document.getElementsByClassName('BJ-actions-add')[0];
     const finishBtn = document.getElementsByClassName('BJ-actions-finish')[0];
 
-    console.log(addCardBtn);
+    const restartBtn = document.getElementsByClassName('BJ-result-restart')[0];
+    const resultTxt = document.getElementsByClassName('BJ-result-txt')[0];
 
-    function card () {
-        return Math.floor(Math.random() * 9 + 2)
-    }
+    const playerCards = document.getElementsByClassName('BJ-playerCards')[0];
 
 
     let playerScore = 0;
     let robotScore = 0;
 
+    function card () {
+        return Math.floor(Math.random() * 9 + 2)
+    }
+
+    function robotGo () {
+        robotScore += card();
+        console.log(`robot score is ${robotScore}`);
+        robotScoreEl.textContent = `Robot score: ${robotScore}`;
+    }
+    function playerGo() {
+        let curCard = card()
+        playerScore += curCard;
+        console.log(`your score: ${playerScore}`);
+        playerScoreEl.textContent = `Your score is ${playerScore}`;
+        showCard(curCard, playerCards)
+    }
+
+    function showCard (card, place) {
+        let cardEl = document.createElement('div');
+        cardEl.className = 'card';
+        cardEl.textContent = card;
+        place.appendChild(cardEl);
+    }
+
+    function clearCards () {
+        playerCards.textContent = '';
+    }
+
+    function robotPlay() {
+        console.log('robot is thinking');
+        while ( robotScore < 18 ) {
+            robotGo();
+        }
+    }
+
+    function result(res) {
+        resultTxt.textContent = `Result: ${res}`
+
+    }
+
+    function compareResults () {
+        if (robotScore > 21) {
+            console.log('You won');
+            result('win');
+        }
+        else if (robotScore < playerScore) {
+            console.log('you won');
+            result('win');
+        }
+        else if (robotScore === playerScore) {
+            console.log('draw')
+            result('draw');
+        }
+        else if (robotScore > playerScore) {
+            console.log('you loose');
+            result('lose');
+        }
+    }
+
     addCardBtn.addEventListener('click', () => {
-        playerScore += card();
-        console.log(`your score is ${playerScore}`);
+        playerGo();
         if(playerScore > 21 ) {
             console.log('you loose');
-            playerScore = 0;
-            console.log(`player score nulled, ${playerScore}`);
+            result('lose');
         }
     })
 
     finishBtn.addEventListener('click', () => {
-        console.log('robot is thinking');
         robotPlay();
-        // compareResults();
+        compareResults();
     })
 
-    function robotPlay() {
-        while ( robotScore < 18 ) {
-            robotScore += card();
-            console.log(`robot score is ${robotScore}`);
-        }
-        if (robotScore > 21) {
-            console.log('You won');
-        }
-        else if (robotScore < playerScore) {
-            console.log('you won');
-        }
-        else if (robotScore == playerScore) {
-            console.log('draw')
-        }
-        else if (robotScore > playerScore) {
-            console.log('you loose');
-        }
+    restartBtn.addEventListener('click', () => {
+        console.log('restart')
         playerScore = 0;
-    }
+        robotScore = 0;
+        robotScoreEl.textContent = 'Robot score: 0';
+        playerScoreEl.textContent = 'Your score: 0';
+        clearCards();
+    })
+
 }
 
 BlackJack();
